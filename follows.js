3d5453b009f6fb38e048ca99d1efae2f27e7253f
Bruce Winter <brucewinter@gmail.com>
2015-02-13 20:48:22 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -13,9 +13,9 @@
 	*/
 
 	var defaultKey		= 'C3Fsyg875-Wkk4_r9lWbvG8g6mKSAKxwbVF3Nkt6cDNMQT0g', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [1389033602], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= [1385833385], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Bruce\'s House', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '1day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
@@ -139,11 +139,7 @@
 										data: points,
 										color: '#' + dataColor
 									});
-								        series.push({
-										name: datastream.id,
-										data: points,
-										color: 'steelblue'
-									});
+								    
 
 									// Initialize Graph DOM Element
 									$('#feed-' + feedId + ' .datastreams .datastream-' + datastream.id + ' .graph').attr('id', 'graph-' + feedId + '-' + datastream.id);
