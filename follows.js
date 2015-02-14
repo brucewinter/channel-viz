57944e6ce7a21558fd7da3524a7970ee6cffda7e
Bruce Winter <brucewinter@gmail.com>
2015-02-13 20:36:00 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -3,19 +3,19 @@
 	/*
 	EXAMPLE CONFIGURATION
 
-		var defaultKey	= 'fje329iun52ngtuijo2f4jeun432A', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [61916,12425,94322], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'My Company\'s Application', // Replaces Xively logo in the header
+		var defaultKey	= 'C3Fsyg875-Wkk4_r9lWbvG8g6mKSAKxwbVF3Nkt6cDNMQT0g', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1389033602], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Bruce\'s House', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= 'C3Fsyg875-Wkk4_r9lWbvG8g6mKSAKxwbVF3Nkt6cDNMQT0g', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1389033602], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Bruce\'s House', // Replaces Xively logo in the header
+		dataDuration	= 'Day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
